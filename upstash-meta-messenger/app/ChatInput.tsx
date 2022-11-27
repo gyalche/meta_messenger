4e5d845f67604bb07f4062fb6e46e9fbd4d4e606
Dawa <sherpadawa01@outlook.com>
2022-11-27 16:23:07 +0545

    working on addMessage api

--- upstash-meta-messenger/app/ChatInput.tsx
+++ upstash-meta-messenger/app/ChatInput.tsx
@@ -1,13 +1,44 @@
 'use client';
 import React, { useState } from 'react';
-
+import { v4 as uuid } from 'uuid';
+import { Message } from '../typing';
 const ChatInput = () => {
   const [input, setInput] = useState('');
+  const addMessage = (e: any) => {
+    e.preventDefault();
+    if (!input) return;
+    const messageToSend = input;
+    setInput(' ');
+
+    const id = uuid();
+    const message: Message = {
+      id,
+      message: messageToSend,
+      created_at: Date.now(),
+      username: 'dawa',
+      profilePic: 'https://www.mypic.com/profile',
+      email: 'dawa@gmail.com',
+    };
+
+    const uploadMessageToUpstash = async () => {
+      const res = await fetch('/api/addMessage', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      const data = await res.json();
+    };
+  };
   return (
-    <form className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-gray-100">
+    <form
+      onSubmit={addMessage}
+      className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-gray-100">
       <input
         type="text"
-        className="flex-1 rounded border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-trasparent px-2 py-3 disabled:opacity-50 disbled:cursor-not-allowed"
+        className="flex-1 rounded border-t border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-trasparent px-2 py-3 disabled:opacity-50 disbled:cursor-not-allowed"
         placeholder="Enter a message"
         value={input}
         onChange={(e) => setInput(e.target.value)}
---
+++ upstash-meta-messenger/pages/api/addMessage.ts
@@ -0,0 +1,26 @@
+import { Message } from './../../typing.d';
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import redis from '../../redis';
+type Data = {
+  message: Message;
+};
+type ErrorData = {
+  body: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | ErrorData>
+) {
+  if (req.method !== 'POST') {
+    res.status(404).json({ body: 'Method Not Allowed' });
+  }
+  const { message } = req.body;
+  //replace the timestamp of the user to the timestamp of the server
+  const newMessage = { ...message, created_at: Date.now() };
+  //push to upstash redis db;
+  await redis.hset('messages', message.id, newMessage);
+
+  res.status(200).json({ message: newMessage });
+}
